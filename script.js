56bc262d0179687cce910ae8ba6552e4bc698476
faboubijou <124534182+faboubijou@users.noreply.github.com>
2023-03-29 16:01:07 +0200

    Basis operand

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(n1,n2) {
+    return n1+n2;
+}
+
+function substract(n1,n2) {
+    return n1-n2;
+}
+
+function multiply(n1,n2) {
+    return n1*n2;
+}
+
+function divide(n1,n2) {
+    if (n2==0) {
+        return "ERROR";
+    } else {
+        return n1/n2;
+    }
+}
+
